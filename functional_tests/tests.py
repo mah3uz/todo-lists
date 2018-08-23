729194b580673473e61c5dc6257a093355f93051
Mahfuz Ahmed <mah3uz@gmail.com>
2018-08-24 00:02:52 +0600

    Mixpanel

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -144,8 +144,21 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('1: Buy Milk', page_text)
 
-
-
-
         # Satisfied, they both go back to sleep
         # self.fail('Finish the test!')
+
+    def test_cannot_add_empty_list_items(self):
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
+
+        # She tries again with some text for the item, which now works
+
+        # Perversely, she now decides to submit a second blank list item
+
+        # She receives a similar warning on the list page
+
+        # And she can correct it by filling some text in
+        self.fail('write me!')
